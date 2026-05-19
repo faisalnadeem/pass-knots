6ae97f6141a100cde05c316a808bd72250d7c5f9
Hassan <xhkhassan9@gmail.com>
2026-05-19 23:12:09 +0500

    Hanlded the redirection to the logged in user to the home page

--- VaultApp/Controllers/HomeController.cs
+++ VaultApp/Controllers/HomeController.cs
@@ -4,5 +4,11 @@ namespace VaultApp.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index() => View();
+    public IActionResult Index()
+    {
+        if (User.Identity?.IsAuthenticated == true)
+            return RedirectToAction("Index", "Vault");
+
+        return View();
+    }
 }
